35d834d190ee9a9b65625272e9ab1414ad7ac304
Lirikman <999gothicman999@mail.ru>
2026-05-15 14:05:29 +0700

    sqlc is connected

---
+++ db/queries/links.sql
@@ -0,0 +1,26 @@
+-- name: GetLink :one
+SELECT id, original_url, short_name, short_url
+FROM links
+WHERE id = $1 LIMIT 1;
+
+-- name: ListLinks :many
+SELECT id, original_url, short_name, short_url 
+FROM links
+ORDER BY id;
+
+-- name: CreateLink :one
+INSERT INTO links (
+original_url, short_name
+) VALUES (
+$1, $2 
+)
+RETURNING id;
+
+-- name: UpdateLink :exec
+UPDATE links
+SET original_url = $2, short_name = $3
+WHERE id = $1;
+
+-- name: DeleteLink :exec
+DELETE FROM links
+WHERE id = $1;
--- main.go
+++ main.go
@@ -1,16 +1,47 @@
 package main
 
 import (
+	generated "code/db/generated"
+	"context"
+	"database/sql"
+	"fmt"
 	"log"
+	"net/http"
+	"os"
 	"time"
 
 	"github.com/getsentry/sentry-go"
 	sentrygin "github.com/getsentry/sentry-go/gin"
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func setupRouter() *gin.Engine {
+func main() {
+	// подключаемся с БД
+	var err error
+	dbUrl := os.Getenv("DATABASE_URL")
+	if dbUrl == "" {
+		log.Fatal("DATABASE_URL не установлена")
+	}
 
+	// Инициализация пула соединений
+	conn, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Ошибка подключения к БД: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+	queries := generated.New(conn)
+	// подключаем мониторинг ошибок
+	errSentry := sentry.Init(sentry.ClientOptions{
+		Dsn:            "https://b81aac0d2c97f7e747a4fb8aeb0d72ea@o4511376473587712.ingest.de.sentry.io/4511376481189968",
+		Debug:          true,
+		SendDefaultPII: true,
+	})
+	if errSentry != nil {
+		log.Fatalf("sentry.Init: %s", errSentry)
+	}
+	defer sentry.Flush(2 * time.Second)
 	// создаём маршрутизатор Gin
 	router := gin.Default()
 	// Once it's done, you can attach the handler as one of your middleware
@@ -20,23 +51,18 @@ func setupRouter() *gin.Engine {
 	// подулючаем логгер
 	router.Use(gin.Logger())
 	// создаём точку входа
-	router.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
+	router.GET("/api/links", func(c *gin.Context) {
+		links, err := queries.ListLinks(c)
+		if err == sql.ErrNoRows {
+			c.JSON(http.StatusNotFound, gin.H{"error": "links not found"})
+			return
+		}
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "database error"})
+			return
+		}
+		c.JSON(http.StatusOK, links)
 	})
-	return router
-}
-
-func main() {
-	err := sentry.Init(sentry.ClientOptions{
-		Dsn:            "https://b81aac0d2c97f7e747a4fb8aeb0d72ea@o4511376473587712.ingest.de.sentry.io/4511376481189968",
-		Debug:          true,
-		SendDefaultPII: true,
-	})
-	if err != nil {
-		log.Fatalf("sentry.Init: %s", err)
-	}
-	defer sentry.Flush(2 * time.Second)
-	router := setupRouter()
 	// запускаем сервер на порту 8080
 	router.Run(":8080")
 }
