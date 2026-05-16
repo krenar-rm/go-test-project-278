25fa7df50ace58c788167aa5be5c4a47a43c8e1d
Lirikman <999gothicman999@mail.ru>
2026-05-16 16:57:21 +0700

    migrations have been changed

--- db/migrations/20260516094657_add_users.sql
+++ db/migrations/20260516094657_add_users.sql
@@ -1,5 +1,4 @@
 -- +goose Up
--- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS links (
 	id BIGSERIAL PRIMARY KEY,
 	original_url TEXT,
@@ -7,10 +6,6 @@ CREATE TABLE IF NOT EXISTS links (
 	short_url TEXT,
 	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
--- +goose StatementEnd
-
 
 -- +goose Down
--- +goose StatementBegin
 DROP TABLE IF EXISTS links;
--- +goose StatementEnd
