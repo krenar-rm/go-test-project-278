86401aed0f3c9fe1f32839980954a773465f7511
Lirikman <999gothicman999@mail.ru>
2026-05-17 18:08:58 +0700

    tests were written using testcontainers

--- main.go
+++ main.go
@@ -17,39 +17,27 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func main() {
-	// подключаемся с БД
-	var err error
-
-	// Инициализация пула соединений
-	conn, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Ошибка подключения к БД: %v\n", err)
-		os.Exit(1)
-	}
-	defer conn.Close()
-	queries := generated.New(conn)
-	// подключаем мониторинг ошибок
-	errSentry := sentry.Init(sentry.ClientOptions{
-		Dsn:            "https://b81aac0d2c97f7e747a4fb8aeb0d72ea@o4511376473587712.ingest.de.sentry.io/4511376481189968",
-		Debug:          true,
-		SendDefaultPII: true,
-	})
-	if errSentry != nil {
-		log.Fatalf("sentry.Init: %s", errSentry)
-	}
-	defer sentry.Flush(2 * time.Second)
-	// создаём маршрутизатор Gin
+// создание маршрутизатора Gin
+func setupRouter() *gin.Engine {
 	router := gin.Default()
-	// Once it's done, you can attach the handler as one of your middleware
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	// подключаем монитор просмотра ошибок
 	router.Use(sentrygin.New(sentrygin.Options{}))
 	// подключаем инструмент восстановления сбоев
 	router.Use(gin.Recovery())
-	// подулючаем логгер
+	// подключаем логгер
 	router.Use(gin.Logger())
-	// маршрут для получения всех записей
-	router.GET("/api/links", func(c *gin.Context) {
-		links, err := queries.ListLinks(c)
+	return router
+}
+
+// получение всех записей
+func listLinks(db *generated.Queries) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		links, err := db.ListLinks(c)
 		if err == sql.ErrNoRows {
 			c.JSON(http.StatusNotFound, gin.H{"error": "links not found"})
 			return
@@ -59,23 +47,29 @@ func main() {
 			return
 		}
 		c.JSON(http.StatusOK, links)
-	})
-	// маршрут для добавления новой записи
-	router.POST("/api/links", func(c *gin.Context) {
+	}
+}
+
+// создание новой записи
+func createLink(db *generated.Queries) gin.HandlerFunc {
+	return func(c *gin.Context) {
 		var link generated.CreateLinkParams
 		if err := c.ShouldBindJSON(&link); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
-		id, err := queries.CreateLink(c, link)
+		id, err := db.CreateLink(c, link)
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
 		c.JSON(http.StatusCreated, id)
-	})
-	// маршрут для изменения записи
-	router.PUT("/api/links/:id", func(c *gin.Context) {
+	}
+}
+
+// обновление записи
+func updateLink(db *generated.Queries) gin.HandlerFunc {
+	return func(c *gin.Context) {
 		var updLink generated.UpdateLinkParams
 		idStr := c.Param("id")
 		id, err := strconv.ParseInt(idStr, 10, 64)
@@ -88,22 +82,25 @@ func main() {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
-		res := queries.UpdateLink(c, updLink)
+		res := db.UpdateLink(c, updLink)
 		if res != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
 		c.JSON(http.StatusOK, res)
-	})
-	// маршрут для получения одной записи
-	router.GET("/api/links/:id", func(c *gin.Context) {
+	}
+}
+
+// получение одной записи
+func getLinkFromId(db *generated.Queries) gin.HandlerFunc {
+	return func(c *gin.Context) {
 		idStr := c.Param("id")
 		id, err := strconv.ParseInt(idStr, 10, 64)
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
-		link, err := queries.GetLink(c, id)
+		link, err := db.GetLink(c, id)
 		if err != nil {
 			c.JSON(http.StatusNotFound, gin.H{
 				"error": "link not found",
@@ -111,24 +108,68 @@ func main() {
 			return
 		}
 		c.JSON(http.StatusOK, link)
-	})
-	// маршрут для удаления записи
-	router.DELETE("/api/links/:id", func(c *gin.Context) {
+	}
+}
+
+// удаление записи
+func deleteLink(db *generated.Queries) gin.HandlerFunc {
+	return func(c *gin.Context) {
 		idStr := c.Param("id")
 		id, err := strconv.ParseInt(idStr, 10, 64)
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
-		err = queries.DeleteLink(c, id)
+		// проверяем наличие записи
+		_, err = db.GetLink(c, id)
 		if err != nil {
 			c.JSON(http.StatusNotFound, gin.H{
-				"error": "link has not been removed",
+				"error": "the link does not exist",
+			})
+			return
+		}
+		// удаляем ссылку
+		err = db.DeleteLink(c, id)
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error": "error deleting links",
 			})
 			return
 		}
 		c.JSON(http.StatusNoContent, id)
+	}
+}
+
+func main() {
+	// подключаемся с БД
+	var err error
+
+	// Инициализация пула соединений
+	conn, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Ошибка подключения к БД: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+	queries := generated.New(conn)
+
+	// подключаем мониторинг ошибок
+	errSentry := sentry.Init(sentry.ClientOptions{
+		Dsn: "https://b81aac0d2c97f7e747a4fb8aeb0d72ea@o4511376473587712.ingest.de.sentry.io/4511376481189968",
 	})
+	if errSentry != nil {
+		log.Fatalf("sentry initialization failed: %s", errSentry)
+	}
+	defer sentry.Flush(2 * time.Second)
+
+	// создаём маршрутизатор
+	r := setupRouter()
+	// регистрируем маршруты
+	r.GET("api/links", listLinks(queries))
+	r.GET("api/links/:id", getLinkFromId(queries))
+	r.POST("api/links", createLink(queries))
+	r.PUT("api/links/:id", updateLink(queries))
+	r.DELETE("api/links/:id", deleteLink(queries))
 	// запускаем сервер на порту 8080
-	router.Run(":8080")
+	r.Run(":8080")
 }
