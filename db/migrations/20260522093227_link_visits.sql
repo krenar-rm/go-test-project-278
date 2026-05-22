641444a558dd4fa842c44a5be2a4841a0016d67c
Lirikman <999gothicman999@mail.ru>
2026-05-22 16:34:50 +0700

    add migration link_visits

---
+++ db/migrations/20260522093227_link_visits.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+CREATE TABLE IF NOT EXISTS link_visits (
+	id BIGSERIAL PRIMARY KEY,
+	ip VARCHAR(45),
+	user_agent VARCHAR(255),
+	referer TEXT,
+	status INT,
+	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+-- +goose Down
+DROP TABLE IF EXISTS link_visits;
