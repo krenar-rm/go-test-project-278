166f6593249d4d71b5cf78c57f3a53f0c04e39a7
Lirikman <999gothicman999@mail.ru>
2026-05-24 18:26:49 +0700

    changed migrations file

---
+++ db/migrations/20260524104410_add_link_and_visits_table.sql
@@ -0,0 +1,26 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS links (
+	id BIGSERIAL PRIMARY KEY,
+	original_url TEXT,
+	short_name TEXT UNIQUE,
+	short_url TEXT,
+	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS link_visits (
+	id BIGSERIAL PRIMARY KEY,
+	link_id BIGINT NOT NULL,
+	ip VARCHAR(45),
+	user_agent VARCHAR(255),
+	referer VARCHAR(500),
+	status INT,
+	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);	
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS link_visits;
+DROP TABLE IF EXISTS links;
+-- +goose StatementEnd
